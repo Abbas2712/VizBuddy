1bd6ab8b03dc291e3856b5afdec4063189fd5a70
Abbas <abbinira2712@gmail.com>
2019-12-10 19:51:01 +0530

    added vector icon, animated background, stylish buttons and input text view and a default logo

--- app/src/main/java/com/example/vizbuddy/login.java
+++ app/src/main/java/com/example/vizbuddy/login.java
@@ -1,14 +1,27 @@
 package com.example.vizbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class login extends AppCompatActivity {
+
+    RelativeLayout myLayout;
+    AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        myLayout = (RelativeLayout) findViewById(R.id.myLayout);
+
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
     }
 }
