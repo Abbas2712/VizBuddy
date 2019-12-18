a5cfb477c4debb9f4685663cf6c3e8c46336c190
Abbas2712 <abbinira2712@gmail.com>
2019-12-18 21:20:08 +0530

    forgot password activity completed

--- app/src/main/java/com/example/vizbuddy/passwordreset.java
+++ app/src/main/java/com/example/vizbuddy/passwordreset.java
@@ -1,21 +1,66 @@
 package com.example.vizbuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class passwordreset extends AppCompatActivity implements View.OnClickListener{
 
+    public TextView noAccountFp;
+    public Button fpbtn;
+    public EditText fpemailid;
     public ImageView backbtn;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passwordreset);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        noAccountFp = (TextView) findViewById(R.id.noAccountFp);
         backbtn = (ImageView) findViewById(R.id.backbtn);
+        fpemailid = (EditText) findViewById(R.id.fpemailid);
+        fpbtn = (Button) findViewById(R.id.fpbtn);
+
+        fpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = fpemailid.getText().toString().trim();
+
+                if (username.equals("")){
+                    Toast.makeText(passwordreset.this, "Enter your email", Toast.LENGTH_SHORT).show();
+                }else{
+                    firebaseAuth.sendPasswordResetEmail(username).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(passwordreset.this, "Password Reset Email Sent!!", Toast.LENGTH_SHORT).show();
+                                finish();
+                                startActivity(new Intent(passwordreset.this, login.class));
+                            }else{
+                                Toast.makeText(passwordreset.this, "Error In Sending Password Reset Email", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 
     @Override
@@ -23,6 +68,11 @@ public class passwordreset extends AppCompatActivity implements View.OnClickList
         if(v == backbtn){
             finish();
             startActivity(new Intent(this, login.class));
+        }
+
+        if(v == noAccountFp){
+            finish();
+            startActivity(new Intent(this, Signup.class));
         }
     }
 }
